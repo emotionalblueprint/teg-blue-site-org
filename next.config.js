286bc8d3b591e9de92a0de078955cc172a017bd0
TEG-Blue – The Emotional Gradient Blueprint <annaparetas@protonmail.com>
2026-06-26 21:59:53 +0200

    Allow Vercel analytics vitals endpoint

--- next.config.js
+++ next.config.js
@@ -543,7 +543,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: blob: https://zenodo.org",
               "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://va.vercel-scripts.com",
+              "connect-src 'self' https://va.vercel-scripts.com https://vitals.vercel-insights.com",
               "frame-src 'self'",
               "object-src 'none'",
               "base-uri 'self'",
